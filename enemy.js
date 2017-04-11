f5a4bb37b926d9214555d5df7a76f85120fe76f1
iinesvuorjoki <iines.vuorjoki@aalto.fi>
2017-04-11 16:24:17 +0300

    lololoo

--- enemy.js
+++ enemy.js
@@ -1,5 +1,47 @@
-var enemy = {
-    x: 2,
-    y: 2,
-    speed: 300
+var enemies = [];
+
+function createEnemies() {
+    
+    var enemy = {
+            x: 2,
+            y: 2,
+            speed: 300
+        };
+    
+    return enemy;
+    
 };
+
+for (i = 0; i < 8; i++) {
+    enemies.push(createEnemies());
+};
+
+function flyingEnemies() {
+    for (i = 0; i < enemies.length[i]; i++) {
+        var flyingEnemy = enemies[i];
+        if (flyingEnemy.x >= 250) {
+            flyingEnemy.direction = 2;
+        }
+        
+        if (flyingEnemy.x <= 10) {
+            flyingEnemy.direction = 1;
+        }
+        if (flyingEnemy.direction === 1) {
+            flyingEnemy.x = flyingEnemy.x + flyingEnemy.speed;
+        }
+        if (flyingEnemy.direction === 2) {
+            flyingEnemy.x = flyingEnemy.x - flyingEnemy.speed;
+        }
+        
+    }
+    
+    var stable = 1,
+        moving = 2;
+    
+    function changeSpeed(speed) {
+        switch (speed) {
+                
+        }
+    };
+    
+
--- player.js
+++ player.js
@@ -1,7 +1,43 @@
 var player = {
     x: 0,
     y: 0,
-    speed: 200
+    speed: 200,
+    direction: 0
 };
 
-var playerLives = 5;
+var playerLives = 5,
+    stable = 1,
+    right = 2,
+    left = 3;
+
+function flyingPlayer(direction) {
+    switch (direction) {
+    case "left":
+        player.x -= player.speed;
+        if (player.x < 20) {
+            player.x = 20;
+        }
+        player.direction = 3;
+        break;
+    case "right":
+        player.x += player.speed;
+        if (player.x > 400) {
+            player.x = 400;
+        }
+        player.direction = 1;
+        break;
+    case "down":
+        player.y += player.speed;
+        if (player.y > 380) {
+            player.y = 380;
+        }
+        player.direction = 2;
+        break;
+    }
+};
+
+function changeSpeed(speed) {
+    switch (speed) {
+            
+    }
+};
